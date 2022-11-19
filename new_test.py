c2c04fb8c8db42221940a38bb9d3d77c203304cb
v-onuphrienko <bkbrad8@gmail.com>
2022-11-19 12:49:10 +0300

    rdy

--- new_test.py
+++ new_test.py
@@ -1,2 +1,8 @@
-import streamlit as st
-st.hello 
+import transformers
+from transformers import pipeline
+
+model_name = "IlyaGusev/rubertconv_toxic_clf"
+pipe = pipeline("text-classification", model=model_name, tokenizer=model_name, framework="pt") 
+
+text = "Ты придурок из интернета"
+print(pipe([text]))
